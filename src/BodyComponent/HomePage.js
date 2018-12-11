53198e725fe177201bd0bc8b950c30adec052089
vincemillora <vcmillora@up.edu.ph>
2018-12-11 15:55:55 +0900

    Home UI changes

--- src/BodyComponent/HomePage.js
+++ src/BodyComponent/HomePage.js
@@ -4,11 +4,11 @@ class HomePage extends Component {
   render() {
     return (
       <div class="homepage">
-        <img class="firstImage" src="Homepage/Ethereum.jpg"></img>
+        <img class="firstImage" src="Homepage/main.jpg"></img>
         <div class="secondImage">
           <br></br>
           <div class="second">
-            <h4>What is CryptoViper</h4>
+            <h4>What is CryptoViper?</h4>
             <p>CryptoViper is a free, interactive code school that teaches you to build games on Ethereum.</p>
             <p>The course is designed for beginners to Vyper and starts off with the absolute basics. So if you've never <br></br>coded with Vyper before, don't worry — I'll walk you through step by step.</p>
           </div>
@@ -20,7 +20,7 @@ class HomePage extends Component {
             <h4 class="colortheme">Interactive Coding Lessons</h4>
             <p>In-browser step-by-step lessons that teaches you the very basics of Vyper. Even by the end of Lesson 1 (which can be completed in one sitting), you'll know enough to officially call yourself a Vyper developer!</p>
           </div>
-          <img class="imagesDesc right" src="Homepage/viper.jpg"></img>
+          <img class="imagesDesc right" src="Homepage/Game.png"></img>
           <div class="right rightDesc">
             <br></br><br></br><br></br>
             <h4 class="colortheme">Build a Viper</h4>
